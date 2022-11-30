2d17bdfda72fc7a481532a60c2b0282aca7a72c7
alcides07 <alcidesdantasdj@gmail.com>
2022-11-30 12:09:22 -0300

    Navbar e Footer

--- src/App.js
+++ src/App.js
@@ -1,24 +1,30 @@
-import logo from './logo.svg';
-import './App.css';
+import {BrowserRouter as Router, Routes, Route} from 'react-router-dom' 
+import Home from "./components/pages/Home"
+import Sobre from "./components/pages/Sobre"
+import Contato from "./components/pages/Contato"
+import NovoProjeto from "./components/pages/NovoProjeto"
+import Projetos from "./components/pages/Projetos"
+
+import Navbar from "./components/layout/Navbar"
+import Footer from "./components/layout/Footer"
+import Container from './components/layout/Container'
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Router>
+
+      <Navbar/>
+      <Container customClass = "min-height"> 
+        <Routes>
+          <Route exact path = "/" element = {<Home/>} > </Route>
+          <Route exact path = "/sobre" element = {<Sobre/>} > </Route>
+          <Route exact path = "/contato" element = {<Contato/>} > </Route>
+          <Route exact path = "/novoprojeto" element = {<NovoProjeto/>} > </Route>
+          <Route exact path = "/projetos" element = {<Projetos/>} > </Route>
+        </Routes>
+      </Container>
+      <Footer/>
+    </Router>
   );
 }
 
---
+++ src/components/layout/Navbar.js
@@ -0,0 +1,22 @@
+import {Link} from 'react-router-dom' 
+import Container from "./Container"
+import styles from "./Navbar.module.css"
+import logo from "../../img/costs_logo.png"
+
+function Navbar (){
+    return (
+        <nav className = {styles.navbar}>
+            <Container> 
+                <Link to = "/" > <img src = {logo} alt = "costs"></img> </Link>
+                <ul className = {styles.lista_itens}>
+                    <li className = {styles.item}> <Link to = "/"> Home </Link> </li>
+                    <li className = {styles.item}> <Link to = "/projetos"> Projetos </Link> </li>
+                    <li className = {styles.item}> <Link to = "/sobre"> Sobre </Link> </li>
+                    <li className = {styles.item}> <Link to = "/contato"> Contato </Link> </li>
+                </ul>
+            </Container>
+        </nav>
+    )
+}
+
+export default Navbar
