d5ff0ca2126f89dc94f106eae0bcad9e520bdd61
alcides07 <alcidesdantasdj@gmail.com>
2022-12-06 10:50:36 -0300

    Tela de  Novo Projeto

--- src/components/pages/NovoProjeto.js
+++ src/components/pages/NovoProjeto.js
@@ -1,6 +1,13 @@
+import FormProjeto from "../projeto/FormProjeto"
+import styles from "./NovoProjeto.module.css"
+
 function NovoProjeto(){
     return (
-        <h1> NovoProjeto </h1>
+        <div className = {styles.novo_projeto_container}>
+            <h1> Criação de Projeto </h1>
+            <p> Crie seu projeto agora mesmo! </p>
+            <FormProjeto btnText = "Criar Projeto"/>
+        </div> 
     )
 }
 
