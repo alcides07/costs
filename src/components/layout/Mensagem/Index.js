da84478765a47560f6ee4c7f459810fcd27d026a
alcides07 <alcidesdantasdj@gmail.com>
2022-12-15 11:05:31 -0300

    Edição de projeto concluida

--- src/components/layout/Mensagem/Index.js
+++ src/components/layout/Mensagem/Index.js
@@ -3,18 +3,19 @@ import {useState, useEffect} from "react"
 
 function Mensagem({tipo, msg}){
 
-    const [visibilidade, setVisibilidade] = useState(false)
+    const [visibilidade, setVisibilidade] = useState(false) 
 
     useEffect(() => {
         if (!msg){
             setVisibilidade(false)
             return 
         }
-
+        
         setVisibilidade(true)
+        
         const tempo = setTimeout(() => {
             setVisibilidade(false)
-        }, 3000)
+        }, 4000)
         
         return () => clearTimeout(tempo)
 
@@ -22,6 +23,7 @@ function Mensagem({tipo, msg}){
 
     return (
         <>
+            {console.log(visibilidade)}
             {visibilidade && (
                 <div className = {`${styles.mensagem} ${styles[tipo]}`}>
                     {msg}
--- src/components/pages/Projeto_Detalhes/Index.js
+++ src/components/pages/Projeto_Detalhes/Index.js
@@ -5,11 +5,15 @@ import styles from "./Styles.module.css"
 
 import Loading from "../../layout/Loading/Index"
 import Container from "../../layout/Container/Index"
+import Form_Projeto from "../../projeto/Form_Projeto/Index"
+import Mensagem from "../../layout/Mensagem/Index"
 
 function Projeto_detalhes(){
     const { id } = useParams()
     const [projeto, set_projeto] = useState([])
     const [form_projeto, set_form_projeto] = useState(false)
+    const [mensagem, set_mensagem] = useState("")
+    const [tipo, set_tipo] = useState("")
 
     useEffect(() => 
         {
@@ -28,6 +32,31 @@ function Projeto_detalhes(){
         }, [id]
     )
 
+    function edicao_form(projeto){
+        // Validação do orçamento 
+        if (projeto.custo > projeto.orcamento){
+            set_mensagem("O custo do projeto não pode exceder o orçamento disponível!")
+            set_tipo("erro")
+            return (false)
+        }
+
+        fetch(`http://localhost:5000/projetos/${projeto.id}`, {
+            method : "PATCH",
+            headers : {
+                "Content-Type" : "application/json",
+            },
+            body : JSON.stringify(projeto),
+        })
+        .then((resposta) => resposta.json())
+        .then((dados) => {
+            set_projeto(dados)
+            set_form_projeto(false)
+            set_mensagem("Projeto atualizado com sucesso!")
+            set_tipo("sucesso")
+        })
+        .catch((erro) => console.log(erro))
+    }
+
     function visualizacao_form_projeto(){
         set_form_projeto(!form_projeto)
     }
@@ -38,6 +67,12 @@ function Projeto_detalhes(){
                 (
                     <div className = {styles.projeto_detalhe}>
                         <Container customClass = "column"> 
+
+                            {mensagem && (
+                                    <Mensagem tipo = {tipo} msg = {mensagem} />
+                                )
+                                
+                            }
 
                             <div className = {styles.detalhes_container}>
                                 <h1> {projeto.nome} </h1>
@@ -70,7 +105,11 @@ function Projeto_detalhes(){
                                 :
                                     (
                                         <div className = {styles.informacoes_projeto}> 
-                                            <p> Edição do Projeto (Form) </p>
+                                            <Form_Projeto
+                                            handle_submit = {edicao_form}
+                                            btnText = "Salvar Edição"
+                                            dados_projeto = {projeto}
+                                            />
                                         </div>
                                     )
                                 }
